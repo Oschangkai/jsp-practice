996fee8ccbe63de89e9766432ab7f8bd2a4d26c5
Oschangkai <oschangkai@kaiyeee.tw>
2019-11-11 13:21:14 +0800

    define route using annotation instate of xml file

--- src/main/java/web/servlets/Hello.java
+++ src/main/java/web/servlets/Hello.java
@@ -4,12 +4,15 @@ import web.module.Book;
 import web.module.BookRepo;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+
+@WebServlet(name="HelloWorld", urlPatterns={"/test2"}, loadOnStartup=1)
 public class Hello extends HttpServlet {
 
     private AtomicInteger _counter = null;
