fe38102ff4742caea12bc7035f519e4dd9b61e42
Oschangkai <oschangkai@kaiyeee.tw>
2019-11-12 15:19:42 +0800

    Use lambda instate of for loop

--- src/main/java/web/servlets/Hello.java
+++ src/main/java/web/servlets/Hello.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @WebServlet(name="HelloWorld", urlPatterns={"/test2"}, loadOnStartup=1)
 public class Hello extends HttpServlet {
@@ -26,18 +27,19 @@ public class Hello extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         final String name = req.getParameter("name");
+//        String[] values = req.getParameterValues("param");  // param=10&param=20&param=30
         int count = _counter.incrementAndGet();
-        StringBuilder books = new StringBuilder();
+        String books;
 
         BookRepo br = new BookRepo();
         List<Book> bookList = br.getAll();
 
-        for (Book b : bookList) {
-            books.append(b.toString());
-        }
+        books = bookList.stream()
+                    .map(Book::toString)
+                    .collect(Collectors.joining(" & "));
 
         req.setAttribute("greeting", String.format("Hello %s!", name));
-        req.setAttribute("books", books.toString());
+        req.setAttribute("books", books);
         req.setAttribute("count", String.valueOf(count));
 
         req.getRequestDispatcher("/books.jsp").forward(req, resp);
